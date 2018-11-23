69ecfc3fd0e1dd3c1a68480efcf9bdcab51393e9
chy9002 <colincai@outlook.com>
2018-11-23 15:12:00 +0800

    fix json format

--- middleware/template.js
+++ middleware/template.js
@@ -3,12 +3,12 @@ const path = require('path');
 const config = require('../config');
 const he = require('he');
 const typeRegrx = /\.(atom|rss)$/;
-const unsupportedRegrx = /\.json$/;
+// const unsupportedRegrx = /\.json$/;
 
 module.exports = async (ctx, next) => {
-    if (ctx.request.path.match(unsupportedRegrx)) {
-        throw Error('<b>JSON output had been removed, see: <a href="https://github.com/DIYgod/RSSHub/issues/1114">https://github.com/DIYgod/RSSHub/issues/1114</a></b>');
-    }
+    // if (ctx.request.path.match(unsupportedRegrx)) {
+    //     throw Error('<b>JSON output had been removed, see: <a href="https://github.com/DIYgod/RSSHub/issues/1114">https://github.com/DIYgod/RSSHub/issues/1114</a></b>');
+    // }
 
     ctx.state.type = ctx.request.path.match(typeRegrx) || ['', ''];
     ctx.request.path = ctx.request.path.replace(typeRegrx, '');
